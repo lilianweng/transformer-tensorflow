59bdb7d5567d1d31053f3a0c40c1a017aff39a9e
Lilian Weng <lilian@openai.com>
2018-10-25 00:41:25 -0700

    tests

--- transformer_test.py
+++ transformer_test.py
@@ -8,7 +8,7 @@ class TransformerTest(tf.test.TestCase):
         self.t = Transformer()
         self.batch_size = 4
         self.seq_len = 5
-        self.input_ph = tf.placeholder(tf.int32, shape=(self.batch_size, self.seq_len))
+        self.raw_input_ph = tf.placeholder(tf.int32, shape=(self.batch_size, self.seq_len))
 
     def test_construct_padding_mask(self):
         with self.test_session() as sess:
@@ -18,8 +18,8 @@ class TransformerTest(tf.test.TestCase):
                 [1, 2, 3, 4, 0],
                 [1, 2, 3, 0, 0],
             ])
-            mask_ph = self.t.construct_padding_mask(self.input_ph)
-            mask = sess.run(mask_ph, feed_dict={self.input_ph: data})
+            mask_ph = self.t.construct_padding_mask(self.raw_input_ph)
+            mask = sess.run(mask_ph, feed_dict={self.raw_input_ph: data})
             expected = np.array([
                 [[1., 1., 1., 1., 1.]] * self.seq_len,
                 [[1., 1., 0., 0., 0.]] * self.seq_len,
@@ -37,9 +37,24 @@ class TransformerTest(tf.test.TestCase):
                           [1, 1, 1, 1, 0],
                           [1, 1, 1, 1, 1]]
             expected = np.array([tri_matrix] * self.batch_size).astype(np.float32)
-            mask_ph = self.t.construct_autoregressive_mask(self.input_ph)
-            mask = sess.run(mask_ph, feed_dict={self.input_ph: data})
+            mask_ph = self.t.construct_autoregressive_mask(self.raw_input_ph)
+            mask = sess.run(mask_ph, feed_dict={self.raw_input_ph: data})
             np.testing.assert_array_equal(mask, expected)
 
     def test_label_smoothing(self):
+        with self.test_session() as sess:
+            ohe = np.array([[
+                [0, 1, 0, 0, 0],
+                [1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1],
+            ]]).astype(np.float)  # (1, 4, 5)
+            out = self.t.label_smoothing(tf.convert_to_tensor(ohe)).eval()
+            expected = np.array([[
+                [0.02, 0.92, 0.02, 0.02, 0.02],
+                [0.92, 0.02, 0.02, 0.02, 0.02],
+                [0.02, 0.02, 0.02, 0.02, 0.92],
+            ]])
+            np.testing.assert_array_equal(out, expected)
+
+    def test_positional_encoding_embedding(self):
         pass
